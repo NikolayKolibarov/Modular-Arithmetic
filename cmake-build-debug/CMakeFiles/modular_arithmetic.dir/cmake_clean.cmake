4e78d7f0055929ce4b34d1aa334035e7eb355502
nikolaykolibarov <nikolaykolibarov@gmail.com>
2018-02-06 09:29:30 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/modular_arithmetic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modular_arithmetic.dir/main.cpp.o"
+  "CMakeFiles/modular_arithmetic.dir/functions.cpp.o"
+  "modular_arithmetic.pdb"
+  "modular_arithmetic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/modular_arithmetic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
